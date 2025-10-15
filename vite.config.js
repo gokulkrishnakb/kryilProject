91618a47741a564bb5d0cd3d61419c06cd51b83c
gokulkrishnakb <gokulkrishnakb070@gmail.com>
2025-10-15 23:51:09 +0530

    Initial project upload

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/kryilProject/', // 👈 important! matches your repo name
+  base: '/', // 👈 important! matches your repo name
 })
